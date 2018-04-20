d520899f5543d04f0fc1d4db8159874bd35dffbc
Artem Dudkin <AVDudkin.SBT@sberbank.ru>
2018-04-20 12:51:30 +0300

    add naive d.ts for TypeScript

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+export function cache(opt: any): any;
+
+export function invalidate_all(): void;
+
+export function register_validator(p0: any, p1: any): any;
+
+export function setStorage(storage: any): void;
+
+export namespace cache {
+    const prototype: {
+    }
+}
+
+export namespace invalidate_all {
+    const prototype: {
+    }
+}
---
+++ lib/storage/asyncStorage.d.ts
@@ -0,0 +1,20 @@
+export function save (id:any, value:any) : any;
+
+export function load (id: any) : any;
+
+export function del (id: any) : any;
+
+export namespace save {
+    const prototype: {
+    }
+}
+
+export namespace load {
+    const prototype: {
+    }
+}
+
+export namespace del {
+    const prototype: {
+    }
+}
