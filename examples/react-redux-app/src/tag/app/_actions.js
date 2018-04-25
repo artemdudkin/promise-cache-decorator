03d5309de6ae0f92c9e08d3827e58067b020bcec
Artem Dudkin <sheeva@list.ru>
2018-04-25 12:01:59 +0300

    formatting

--- examples/react-redux-app/src/tag/app/_actions.js
+++ examples/react-redux-app/src/tag/app/_actions.js
@@ -9,30 +9,30 @@ export const FORECAST_OK            = "FORECAST_OK";
 export const FORECAST_FAIL          = "FORECAST_FAIL";
 
 const _getWeather = (town, country) => {
-	const ts = Date.now();
-	return api.getWeather(town, country)
-			.then(res => {
-				res.data.ts = Date.now() - ts;
-				return res;
-			})	
+  const ts = Date.now();
+  return api.getWeather(town, country)
+	 .then(res => {
+           res.data.ts = Date.now() - ts;
+           return res;
+         })	
 }
 
 const _load = (dispatch, town, country) => {
-	return cache({
-		type:"age",
-		maxAge:20000,
-		id:"weather",
-		tardy : () => {dispatch({type:FORECAST_LOCK})},
-		tardy_timeout : 200
- 	})(_getWeather)(town, country);
+  return cache({
+           type:"age",
+           maxAge:20000,
+           id:"weather",
+           tardy : () => {dispatch({type:FORECAST_LOCK})},
+           tardy_timeout : 200
+         })(_getWeather)(town, country);
 }
 
 export const load = (town, country) => (dispatch, getState) => {
-	_load(dispatch, town, country).then(res => {
-		dispatch({type:FORECAST_OK, data:res.data});
-	})
-  	.catch( err => {
-		dispatch({type:FORECAST_FAIL, error:err});
-  	});
+  _load(dispatch, town, country).then(res => {
+    dispatch({type:FORECAST_OK, data:res.data});
+  })
+  .catch( err => {
+    dispatch({type:FORECAST_FAIL, error:err});
+  });
 }
 
