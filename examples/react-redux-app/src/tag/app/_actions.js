b43259f1c7ceeca6252f56491c2a3c48cb6f8848
Artem Dudkin <AVDudkin.SBT@sberbank.ru>
2018-05-07 12:37:44 +0300

    fix example

--- examples/react-redux-app/src/tag/app/_actions.js
+++ examples/react-redux-app/src/tag/app/_actions.js
@@ -1,8 +1,8 @@
 import api from '../../api';
 
-import {cache, setSettings} from 'promise-cache-decorator';
+import cache from 'promise-cache-decorator';
 import storage from 'promise-cache-decorator/lib/storage/localStorage';
-setSettings({storage});
+cache.setSettings({storage});
 
 export const FORECAST_LOCK          = "FORECAST_LOCK";
 export const FORECAST_OK            = "FORECAST_OK";
@@ -12,23 +12,31 @@ const _getWeather = (town, country) => {
   const ts = Date.now();
   return api.getWeather(town, country)
 	 .then(res => {
+           //for example, add something to result of api.getWeather
            res.data.ts = Date.now() - ts;
            return res;
          })	
 }
 
-const _load = (dispatch, town, country) => {
-  return cache({
-           type:"age",
-           maxAge:20000,
-           id:"weather",
-           tardy : () => {dispatch({type:FORECAST_LOCK})},
-           tardy_timeout : 200
-         })(_getWeather)(town, country);
-}
-
 export const load = (town, country) => (dispatch, getState) => {
-  _load(dispatch, town, country).then(res => {
+/*  
+  _getWeather(town, country)
+  .then(res => {
+    dispatch({type:FORECAST_OK, data:res.data});
+  })
+  .catch( err => {
+    dispatch({type:FORECAST_FAIL, error:err});
+  });
+*/
+
+  cache({
+    type:"age",
+    maxAge:20000,
+    id:"weather",
+    tardy : () => {dispatch({type:FORECAST_LOCK})},
+    tardy_timeout : 200
+  })(_getWeather)(town, country)
+  .then(res => {
     dispatch({type:FORECAST_OK, data:res.data});
   })
   .catch( err => {
