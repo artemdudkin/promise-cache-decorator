e1b31377f97f448e2405a42179da233b5e8e66d2
Artem Dudkin <AVDudkin.SBT@sberbank.ru>
2018-05-07 12:29:14 +0300

    fix d.ts

--- lib/storage/asyncStorage.d.ts
+++ lib/storage/asyncStorage.d.ts
@@ -1,20 +1,3 @@
 export function save (id:any, value:any) : any;
-
 export function load (id: any) : any;
-
 export function remove (id: any) : any;
-
-export namespace save {
-    const prototype: {
-    }
-}
-
-export namespace load {
-    const prototype: {
-    }
-}
-
-export namespace remove {
-    const prototype: {
-    }
-}
